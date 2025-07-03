a0b5d886c6f0d85805494116d2e923a3a8b92824
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-07-03 19:56:44 +0530

    Add files via upload

---
+++ INCRIQ.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		
+		     int j=sc.nextInt();
+		     int s=0;
+		     s=j+7;
+		     if(s>170)
+		     {
+		         System.out.println("Yes");
+		     }
+		     else
+{
+		         System.out.println("No");
+		     }
+		     
+		 
+	}
+}
