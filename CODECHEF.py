250bb17d39a7ae9afedc89c489957177e0f47a75
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2025-10-10 21:42:16 +0530

    Add files via upload

---
+++ CODECHEF.py
@@ -0,0 +1,17 @@
+# cook your dish here
+N = int(input())
+M = N %7
+if ( M == 1):
+    print("NO")
+elif(M == 2):
+    print("NO")
+elif(M == 3):
+    print("NO")
+elif(M == 4):
+    print("Yes")
+elif(M == 5):
+    print("NO")
+elif(M == 6):
+    print("NO")
+else:
+    print("No")
