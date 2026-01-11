664afe113282cc54b17a1bc0508c48632fcc03ce
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2026-01-11 19:47:49 +0530

    Add files via upload

--- WAPEN.py
+++ WAPEN.py
@@ -1,7 +1,6 @@
 # cook your dish here
-x,y=map(int,input().split())
-if 1<=x<=150 and 0<=y<=10:
-    y=y*10
-    c=x+y
-    
-print(c)
+a,b=map(int,input().split())
+if b==0:
+    print(a)
+else:
+    print(a+b*10)
