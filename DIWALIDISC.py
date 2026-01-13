2b6467c4d44a8eab2daf2d94a6c9baab85d9323d
G.Durga Anantha Lakshmi <166210110+AnanthaLakshmi24@users.noreply.github.com>
2026-01-13 18:36:32 +0530

    Add files via upload

--- DIWALIDISC.py
+++ DIWALIDISC.py
@@ -1,16 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-	// your code goes here
-	int x,y;
-	cin >> x >> y;
-	if (x>y){
-	    int z=x-y;
-	    cout << z;
-	}
-	else{
-	    cout << 0<< endl;
-	}
-
-}
+# cook your dish here
+a,b=map(int,input().split())
+if a>b:
+    print(a-b)
+else:
+    print(0)
